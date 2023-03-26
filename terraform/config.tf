f2526f4b78391da0343d2eee608706747e0b6b00
Channabasavayya <nmchannabasavayya@gmail.com>
2023-03-27 01:32:10 +0530

    encapsulated keys

--- terraform/config.tf
+++ terraform/config.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIA4S4LV7IUYGAVR2BP"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
