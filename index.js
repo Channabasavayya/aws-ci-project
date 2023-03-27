bc7ac133a1061402dc4311d2678e5a54021527c4
Channabasavayya <nmchannabasavayya@gmail.com>
2023-03-28 00:19:33 +0530

    edited index file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res){
-    res.send("Hello World from India");
+    res.send("Hello all from India");
 });
 
 app.listen(80);
